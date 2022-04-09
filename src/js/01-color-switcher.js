0753c6e2aba4d4ba7ee27bc7fce1945b7abc90dd
TravetskaN <ntravetskaya@gmail.com>
2022-04-09 21:31:43 +0100

    ДЗ-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+timeInetrval = null;
+
+startButton.addEventListener('click', startChengeColor);
+
+function startChengeColor(event) {
+  timeInetrval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startButton.disabled = true;
+}
+
+stopButton.addEventListener('click', stopChenge);
+
+function stopChenge(event) {
+  if (timeInetrval) {
+    clearInterval(timeInetrval);
+    startButton.disabled = false;
+  }
+}
